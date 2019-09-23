07dbdfec4468af12c9361ca92c19325351713fd9
bustillo.nikko@yahoo.com <bustillo.nikko@yahoo.com>
2019-09-23 12:08:42 -0400

    9/23/19 update

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/styles.css', express.static(path.join(__dirname, '/styles.css')));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    alert('choose your Pokémon');
+});
+
+app.get('/bulbasaur', (res,req) => {
+    res.sendFile(path.join(__dirname, 'bulbasaur.html'));
+    alert('you chose Bulbasaur!');
+});
